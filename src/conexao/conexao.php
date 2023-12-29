e4312f5d49d00933cf5317b9a82d0f535a2fed40
alisonnRB <alisonrbjs@gmail.com>
2023-12-28 23:17:51 -0300

    poo server

---
+++ src/conexao/conexao.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once '../resposta/resposta.php';
+
+class Connection extends Respost
+{
+    protected $conect;
+    private $state;
+
+    public function createConnection()
+    {
+        try {
+            $this->conect = new PDO("mysql:host=localhost;dbname=palabraria", "root", "");
+            $this->setState(true);
+        } catch (PDOException $e) {
+            $this->setState(false);
+        }
+
+        if ($this->state) {
+            return $this->conect;
+        } else {
+            $erro = new Respost(500, false, 'Impossivel conectar ao servidor');
+            $erro->Return();
+        }
+    }
+
+    protected function GetConect()
+    {
+        return $this->conect;
+    }
+
+    protected function GetState()
+    {
+        return $this->state;
+    }
+
+    protected function setState($value)
+    {
+        $this->state = $value;
+    }
+}
