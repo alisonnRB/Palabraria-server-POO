245a59f17b9fe06a167a0a4e9c6798e920d6c2d2
alisonnRB <alisonrbjs@gmail.com>
2024-03-07 09:40:12 -0300

    search mode adjust

--- src/controls/search.php
+++ src/controls/search.php
@@ -9,6 +9,9 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 $type = $_GET['tipo'];
 $search = $_GET['busca'];
 $index = $_GET['index'];
-$bd = $_GET['bd'] ? $_GET['bd'] : null;
+if (isset($_GET['bd'])) {
+    $bd = $_GET['bd'];
+    $busca = new Search($type, $search, $index, $bd);
+}
 
-$busca = new Search($type, $search, $index, $bd);
+$busca = new Search($type, $search, $index);
