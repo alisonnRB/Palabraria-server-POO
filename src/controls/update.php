2df61d7e1332097104a5ea9a936a8aa5024ae9f8
alisonnRB <alisonrbjs@gmail.com>
2024-03-13 21:17:05 -0300

    request auth complete

--- src/controls/update.php
+++ src/controls/update.php
@@ -2,12 +2,23 @@
 
 
 include_once '../Update/Update.php';
+include_once '../resposta/resposta.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, PATCH');
 header('Access-Control-Allow-Headers: *');
 
 $method = $_SERVER['REQUEST_METHOD'];
+$token = $_SERVER['HTTP_AUTHORIZATION'];
+
+
+$token = str_replace('Bearer ', '', $token);
+$login = new Login(false, false, $token);
+
+if($login->getAuth()->tipo != "admin" || $login->getAuth()->tipo != "moder"){
+    $res = new Respost(200, false, "não autorizado");
+    $res->Return();
+}
 
 if($method == "DELETE" || $method == "PUT"){
     $form = [
